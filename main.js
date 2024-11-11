a65438758c1ea925038cacf2e0226ab1a0fe34c5
Pervirna2017 <148382958+Pervirna2017@users.noreply.github.com>
2024-11-11 19:59:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+/*
+const edadUsuario = prompt("Ingresa tu edad");
+let edad>=18;
+if(edadUsuario === "20"){
+    console.log("Podes entrar");
+
+} 
+else{
+console.log("No podes entrar al sitio");
+}
+
+/*
+for (let i=1; i<=5; i= i+1){
+    console.log(i)
+}
+
+i     i<5     i=i+1
+1     v         2
+2     v         3
+3     v         4
+4     v         5 
+5     v         6 
+6     f
+
+
+
+let entrada = prompt("ingrese el numero 12")
+while(entrada !="ESC") {
+    alert( "el usuario ingresó"+entrada);
+    entrada = prompt("ingresar otro numero");
+    alert("Muchas gracias!!");
+}
+    
+*/
+let numeroPar= parseInt(prompt("Ingrese un numero par"))  
+
+if(esPar){
+    console.log ("El numero es par")
+}
+else{ 
+    console.log ("El numero no es par")
+}
